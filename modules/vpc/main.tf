f6ede14bb7554f1849b9c5432b3c79f5995635ba
Yossi-Spindler_verint <yossi.spindler@verint.com>
2026-02-16 10:48:30 +0200

    l3-mgw

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -7,10 +7,6 @@ data "aws_availability_zones" "available" {
 	}
 }
 
-resource "random_string" "suffix" {
-	length  = 8
-	special = false
-}
 
 module "vpc" {
 	source  = "terraform-aws-modules/vpc/aws"
